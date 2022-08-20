51366881d2aacfa2dc1dd32b04c392ad1276be0f
stkirk <shaanekirk@gmail.com>
2022-08-20 15:10:49 -0400

    created method on custom map class to add a marker with argument passing interface

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,3 +1,15 @@
+import { User } from "./User"; // not needed with interface usage for addMarker
+import { Company } from "./Company"; // not needed with interface usage for addMarker
+// can do two things with a class in TS, create a new instance of that class or use it to annotate a type of object
+
+// Define an interface on how an object can be an argument to the addMarker method on our CustomMap class --> puts the onus on User, Company, Park, CarLot etc to conform to this structure
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
 // Restrict what can be done to the google Map class object so that other developers don't call unvetted methods that would break our application
 // By that we mean create a custom map class, exposing only bare bones methods and props that we intend for the application to use
 // Hide the existence of the google map from other devs by marking the custom class as private
@@ -18,5 +30,37 @@ export class CustomMap {
         },
       }
     );
+  }
+
+  // These two methods aren't ideal, lots of duplication
+  // Instead of doing it this way, we can combine into one method and define an interface that works for a User and Company object
+  addUserMarker(user: User): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: user.location.lat,
+        lng: user.location.lng,
+      },
+    });
+  }
+  addCompanyMarker(company: Company): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: company.location.lat,
+        lng: company.location.lng,
+      },
+    });
+  }
+
+  // Single method that accepts a 'mappable' object that is gatekept by our interface to make sure it has all the properties we need to addMarker
+  addMarker(mappable: Mappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
   }
 }
