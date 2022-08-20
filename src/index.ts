04e494a0e25df389b759492e8c07b370795b6706
stkirk <shaanekirk@gmail.com>
2022-08-20 13:20:21 -0400

    added google map class instance centered on 0,0 to render in maps div

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
+/// <reference types="@types/google.maps" />
 import { User } from "./User";
 import { Company } from "./Company";
 
-const user = new User();
-const company = new Company();
-
-console.log(user);
-console.log(company);
+//optional argument signified by opts? in hovering over .Map class
+// go to docs with cmd click and find we can pass in an object that looks like MapOptions interface which is defined in the google maps type definition file
+new google.maps.Map(document.getElementById("map") as HTMLElement, {
+  zoom: 1,
+  center: { lat: 0, lng: 0 },
+});
